2cf0f7b176f9099621c413cc64339832a11c994c
Faiz Dev <dev@faiz-dev.tpgi.com.au>
2021-05-18 23:31:46 +1000

    Averaging function and coinspot api

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-import testModules
+from testModules import get_prices, store_price, test_buy, test_sell, average_prices_hourly, get_latest_custom
 from time import sleep
 from time import gmtime, strftime
 import threading
 
-prices = {}  # Historical Data
-initialCapital = 0  # Test Cash
 
-
-def main_thread():
-    print("Hello, friend.")
+def main_t():
+    get_latest_custom()
 
 
 # Driver
 if __name__ == '__main__':
-    print('--- Welcome to The Stonks Markets ---')
+    print('--- Welcome to TSM ---')
 
 
-def scheduler():
-    # Store price at 10s
+def scheduler_t():
+    # Store price and averages
     while True:
-        testModules.store_price(strftime('%Y-%m-%d %H:%M:%S', gmtime()), prices)
-        print(prices)
+        # Generate historical data
+        store_price(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
+        # Generate Hourly Averages
+        if bool(get_prices() is False):
+            print("Dict empty!")
+        # else:
+            # print("Average price (Past Hour): " + str(average_prices_hourly()))
+
         sleep(10)
 
 
-b = threading.Thread(name='scheduler', target=scheduler)
-f = threading.Thread(name='foreground', target=main_thread)
+b = threading.Thread(name='scheduler', target=scheduler_t)
+f = threading.Thread(name='foreground', target=main_t)
 
 b.start()
 f.start()
--- testModules.py
+++ testModules.py
@@ -1,11 +1,26 @@
+from coinspot import CoinSpot
+import config
 import requests
-import json
-import redis
+
+_api_key = config._api_key
+_api_secret = config._api_secret
+
+coin = CoinSpot(_api_key, _api_secret)
+prices = {}  # Historical Data
+capital = 0  # Test Cash
+netProfitLoss = 0  # Positive = profit, Negative = loss
 
 
 def get_latest_eth():
     cs_prices = requests.get('https://www.coinspot.com.au/pubapi/latest/ETH').json()['prices']
-    return cs_prices['ask']
+    return cs_prices['last']
+
+
+def get_latest_custom():
+    try:
+        print(coin.my_balances())
+    except ValueError:
+        print('JSON decoding has failed')
 
 
 def get_latest_prices():
@@ -14,23 +29,40 @@ def get_latest_prices():
 
 
 # Generate historical data
-def store_price(currtime, storage):
-    storage.__setitem__(currtime, get_latest_eth())
+def store_price(currtime):
+    prices.__setitem__(currtime, get_latest_eth())
+
+
+def average_prices_hourly():
+    prices_hour_past = [key for key in prices if '2021-05-18 10:' in key.lower()]  # Filter timestamp keys
+    hourly_sum = 0
+    for key in prices_hour_past:
+        hourly_sum += float(prices[key])
+    return hourly_sum / len(prices_hour_past)
 
 
 # Devise MACD Strategy
 def should_buy():
-    return True
+    pass
 
 
 def should_sell():
-    return True
+    pass
 
 
 # Getters and Setters
 def get_current_profits():
-    return 0
+    pass
 
 
-def get_current_units(symbol):
-    return 0
+def get_prices():
+    return prices
+
+
+# Test Modules
+def test_buy(capital, netpl):
+    pass
+
+
+def test_sell():
+    pass
