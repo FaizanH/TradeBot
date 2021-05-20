a42320138ffeaf22f9df9770e94c0f02fef042cd
Faizan <faizanh53@gmail.com>
2021-05-21 03:07:52 +1000

    Implmented trailing block percentage feature

--- main.py
+++ main.py
@@ -18,26 +18,25 @@ if __name__ == '__main__':
 def scheduler_t():
     prices = []
     initialprice = float(get_latest_eth())  # Needs to have atleast 2 elements to comapare
-    intervalcount = 0
+
+    sleep(5)
+
+    intervalcount = 2.5
     pchange = 0
     print('- Running 2.5/15 minute tests')
-    while True:
-        # For every 2.5 mins
-        # Save initial price
-        # After 15 min replace initial price with current price
-        # or
-        # Save price in list
-        # After 15 mins set trailing price
 
+    while True:
+        # After 15 mins set trailing price
+        # Append to file instead or using sliding window approach
         prices.append(float(get_latest_eth()))
         pchange = get_latest_pchange_custom(initialprice, prices[-1])
-
         print(str(intervalcount)+' min' '- $' + str(prices[-1]) + '| % Change = ' + str(pchange))
+
+        if intervalcount > 15:
+            initialprice = prices[-5]
         if intervalcount % 15 == 0 and intervalcount != 0:
             print('15 min mark - new reset')
-            initialprice = prices[-1]
 
-        # print('- Running get_latest_percent_change_1h test')
         if should_buy(pchange, intervalcount) and intervalcount > 2.5:
             print('Target % met. Buying...')
 
@@ -47,7 +46,7 @@ def scheduler_t():
         intervalcount += 2.5
         sleep(5)  # Check at 2.5 minute intervals
 
-       # store_price(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
+# store_price(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
 
 
 b = threading.Thread(name='scheduler', target=scheduler_t)
