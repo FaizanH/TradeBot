b97e5e18d7656b6f617873b8176d229aaaa69b6d
Faizan Hussain <FaizanH@BVDomain.local>
2021-05-21 13:17:15 +1000

    First block works and added buy/sell checks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from testModules import get_latest_pchange_custom, get_latest_eth, should_buy, should_sell, test_buy, test_sell
+from testModules import percent_change_custom, get_latest_eth, should_buy_sell_wait, test_buy, test_sell
 from time import sleep
 from time import gmtime, strftime
 import threading
@@ -6,44 +6,49 @@ import threading
 
 print('Price initialisation successful')
 
+
 def main_t():
     pass
 
 
 # Driver
 if __name__ == '__main__':
-    print('--- Welcome to TSM ---')
+    print('--- Welcome to TSM ---\n')
 
 
 def scheduler_t():
+    interval = 0
+    p = 0
     prices = []
-    initialprice = float(9999)  # Needs to have atleast 2 elements to comapare
+    startpr = float(get_latest_eth())
 
     sleep(5)
 
-    intervalcount = 2.5
-    pchange = 0
-    print('- Running 2.5/15 minute tests')
-
+    print('- RUNNING 2.5/15 TESTS\n')
+    print('TIME | $PRICE | % Δ 10MIN')
     while True:
-        # After 15 mins set trailing price
         # Append to file instead or using sliding window approach
         prices.append(float(get_latest_eth()))
-        pchange = get_latest_pchange_custom(initialprice, prices[-1])
-        print(str(intervalcount)+' min' '- $' + str(prices[-1]) + '| % Change = ' + str(pchange))
+        p = percent_change_custom(startpr, prices[-1])
+        if interval == 0:
+            print(str(interval) + ' | ' + str("{:.8}".format(prices[-1])) + ' | ' + str(0))
+        else:
+            print(str(interval) + ' | ' + str("{:.8}".format(prices[-1])) + ' | ' + str("{:.4}".format(p)))
 
-        if intervalcount > 15:
-            initialprice = prices[-5]
-        if intervalcount % 15 == 0 and intervalcount != 0:
-            print('15 min mark - new reset')
+        if interval >= 10:  # Set trail start
+            startpr = prices[-4]
+        if interval % 10 == 0 and interval != 0:
+            print('TRAIL PRICE SET: LAST 10MIN')
 
-        if should_buy(pchange, intervalcount) and intervalcount > 2.5:
-            print('Target % met. Buying...')
+        outcome = should_buy_sell_wait(p, interval)
+        if outcome == 'buy' and interval > 2.5:
+            print('Buying...')
+        elif outcome == 'sell':
+            print('Selling...')
+        elif outcome == 'wait':
+            print('Waiting...')
 
-        if should_sell():
-            print('Trailing stop loss conditions met. Selling...')
-
-        intervalcount += 2.5
+        interval += 2.5
         sleep(5)  # Check at 2.5 minute intervals
 
 # store_price(strftime('%Y-%m-%d %H:%M:%S', gmtime()))
--- testModules.py
+++ testModules.py
@@ -3,9 +3,8 @@ import config
 import requests
 import json
 
-_api_key = config._api_key
-
-cmc = CoinMarketCapAPI(_api_key)
+# _api_key = config._api_key
+# cmc = CoinMarketCapAPI(_api_key)
 
 prices = {}  # Historical Data
 percentages_history_1h = []
@@ -13,51 +12,61 @@ capital = 0  # Test Cash
 netProfitLoss = 0  # Positive = profit, Negative = loss
 
 
-def get_latest_pchange_custom(initialprice, currentprice):
-    pchange = ((currentprice - initialprice)/initialprice) * 100
-    return pchange
+def percent_change_custom(pinitial, pcurrent):
+    p = ((pcurrent - pinitial)/pinitial) * 100
+    return p
 
-def get_latest_pchange_1h():
-    try:
-        cmc_data = cmc.cryptocurrency_listings_latest().data
-        for x in cmc_data:
-             if (x['symbol'] == 'ADA'):
-                 p = json.loads(json.dumps(x))['quote']['USD']['percent_change_1h']
-                 return p
-    except ValueError:
-        print('JSON decoding has failed')
+
+# def get_latest_percent_1h():
+#     try:
+#         cmc_data = cmc.cryptocurrency_listings_latest().data
+#         for x in cmc_data:
+#             if x['symbol'] == 'ADA':
+#                 p = json.loads(json.dumps(x))['quote']['USD']['percent_change_1h']
+#                 return p
+#     except ValueError:
+#         print('JSON decoding has failed')
+
 
 # Devise MACD Strategy
-def should_buy(pchange, interval):
-    # Buy if percentage > 5
-    # if get_latest_pchange_1h() > 5:
-    if pchange > 5 and interval <= 15:
-        print("Percentage increased by 5% in 15 minutes")
-        return True
-    elif pchange > 15 and interval > 15:
-        print("Percentage increased by 15% took longer than 15 minutes")
-        return True
-    return False
+
+
+def should_buy_sell_wait(p, interval):
+    if 15 > p > 5:
+        print("Percentage INCREASE 5-15% over " + str(interval) + "min")
+        return 'buy'
+    elif p > 15:
+        print("Percentage INCREASE over 15% over " + str(interval) + "min")
+        return 'buy'
+    elif p <= -10:
+        print("Percentage DROP >= -10% over " + str(interval) + "min")
+    return ''
+
 
 def should_sell():
-    if get_latest_pchange_1h() < -10:
-        print("Percentage just dropped below -10% in last hour")
-        return True
+    # if get_latest_percent_1h() < -10:
+    #     print("Percentage just dropped below -10% in last hour")
+    #     return True
     return False
+
 
 # Getters and Setters
 def get_current_profits():
     pass
 
+
 def get_percentages_1h():
     return percentages_history_1h
+
 
 def get_prices():
     return prices
 
+
 # Test Modules
 def test_buy(capital, netpl):
     pass
+
 
 def test_sell():
     pass
@@ -69,12 +78,15 @@ def get_latest_eth():
     cs_prices = requests.get('https://www.coinspot.com.au/pubapi/latest/ETH').json()['prices']
     return cs_prices['ask']
 
+
 def get_latest_prices():
     response = requests.get('https://www.coinspot.com.au/pubapi/latest')
     return response.text
 
+
 def store_price(currtime):
     prices.__setitem__(currtime, get_prices())
+
 
 def average_prices_hourly():
     prices_hour_past = [key for key in prices if '2021-05-18 10:' in key.lower()]  # Filter timestamp keys
